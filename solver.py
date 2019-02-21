f9c3a0a1f120619cc0722b81d7027f35cb5a5c54
Matheus Osório <matheuscerqueir@gmail.com>
2019-02-21 02:04:59 -0300

    made some additions, but the thing is still broken

--- solver.py
+++ solver.py
@@ -1,4 +1,12 @@
 import numpy as np 
+#0 0 6 0 3 0 0 0 0 0 0 5 0 0 0 0 0 3 0 0 0 0 4 9 2 0 7 7 0 0 0 0 0 0 3 0 0 9 0 2 0 0 7 0 0 0 0 0 0 0 3 0 9 0 0 0 4 0 0 0 0 0 0 0 6 0 1 0 0 4 0 0 0 0 0 0 0 0 5 6 1
+
+
+
+def make_sudoku():
+    sudoku = list(input("type sudoku: "))
+    sudoku = list(filter(lambda n: n!=' ',sudoku))
+    return np.array(sudoku).reshape(9,9).astype(np.int)
 
 
 def solved(sudoku):
@@ -8,35 +16,61 @@ def solved(sudoku):
                 return False
     return True
 
-def unable_to_solve(possibilities):
+
+def unable_to_solve(sudoku,possibilities):
     for k in range(9):
         for j in range(9):
             sum = 0
-            for i in range(9):
-                sum += possibilities[i][j][k]
+            if sudoku[j][k]==0:
+                for i in range(9):
+                    sum += possibilities[i][j][k]
                 if sum==0:
                     return True
     return False
 
+
 def unique(sudoku, number, x, y):
-    if sudoku[:][y].any() == number:
+    if np.any(sudoku[:][y]==number):
         return False
-    if sudoku[x][:].any() == number:
+    if np.any(sudoku[x][:] == number):
         return False
     aux_x = x - x%3
     aux_y = y - y%3
-    if sudoku[aux_x:aux_x+3][aux_y:aux_y+3].any() == number:
+    if np.any(sudoku[aux_x:aux_x+3][aux_y:aux_y+3] == number):
         return False
     
     return True
 
+
 def make_possibilities(sudoku):
-    possibilities = np.zeros(81*9).reshape(9,9,9)
+    possibilities = np.zeros(81*9).reshape(9,9,9).astype(np.int)
     for i in range(9):
         for j in range(9):
             for k in range(9):
-                if unique(sudoku,i+1,j,k):
+                if unique(sudoku,i+1,j,k) and sudoku[j][k]==0:
                     possibilities[i][j][k]=1
+    return possibilities
+
+
+def only_possibility(sudoku,possibilities):
+    value = False
+    for i in range(9):
+        for j in range(9):
+            sum=0
+            for k in range(9):
+              sum+= possibilities[k][i][j]
+            if sum==1:
+                for k in range(9):
+                    if possibilities[k][i][j]==1:
+                        sudoku[i][j]=k+1
+                        possibilities = make_possibilities(sudoku)
+                        value = True
+                        break
+    if value:
+        print("safado")
+        return only_possibility(sudoku,possibilities)
+    return sudoku
+
 
 def delete(possibilities,num,x,y):
     for i in range(9):
@@ -47,28 +81,37 @@ def delete(possibilities,num,x,y):
     aux_x = x - (x%3)
     aux_y = y - (y%3)
 
-    for i in range(aux_x,aux_x + 3):
-        for j in range(aux_y,aux_y + 3):
+    for i in range(aux_x,(aux_x + 3)):
+        for j in range(aux_y,(aux_y + 3)):
             possibilities[num][i][j]=0
     
     return possibilities
 
-def solver(sudoku,possibilities):
 
+def solver(sudoku,possibilities,n):
+    print(n)
     if solved(sudoku):
         return sudoku
     
-    if unable_to_solve(possibilities):
+    if unable_to_solve(sudoku,possibilities):
         return 0
     
     for i in range(9):
         for j in range(9):
             if(sudoku[i][j]==0):
-                copy = sudoku.copy()
                 for k in range(9):
+                    #print("({},{}): {}".format(i,j,k))
                     if possibilities[k][i][j]==1:
-                        copy[i][j] = k+1
-                        copy_p = delete(possibilities.copy(),k,i,j)
-                        result = solver(copy,copy_p)
-                        if result != 0:
+                        copy = sudoku.copy()
+                        copy[i][j]=k+1
+                        copy_p = make_possibilities(copy)
+                        sudoku = only_possibility(copy,copy_p)
+                        result = solver(copy,copy_p,n+1)
+                        if type(result) != int:
                             return result
+    return 0
+
+
+sudoku = make_sudoku()
+helper = make_possibilities(sudoku)
+print(solver(sudoku,helper,0))
